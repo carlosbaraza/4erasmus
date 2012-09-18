4a711e2dc851559712b4340c8a6da6c368ca8c8f
Carlos Baraza Haro <demon.xel@gmail.com>
2012-09-18 02:43:13 +0100

    mainPage modify

--- application/views/mainPage_view.php
+++ application/views/mainPage_view.php
@@ -4,8 +4,23 @@
 
 */ ?>
 
-<?php 
 
-	echo "Hola amigos!";
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="extras/css/mainPage.css">
+	</head>
+	<body>
+		<div id="container">
+			<div id="leftContainer">
 
-?>
+			</div>
+			<div id="rightContainer">
+				<header>
+
+				</header>
+			</div>
+		</div>
+	</body>
+</html>
