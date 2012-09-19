9254f80027056800e963128216816a1be1c4cc4c
Carlos Baraza Haro <demon.xel@gmail.com>
2012-09-19 05:32:30 +0100

    JQuery and calendar add

--- application/controllers/mainPage.php
+++ application/controllers/mainPage.php
@@ -4,9 +4,20 @@ class mainPage extends CI_Controller {
 
 	public function index()
 	{
+<<<<<<< HEAD
 		$this->template->js('common/jquery');
+=======
+		//JQuery + JQuary UI with Flick Theme
+		$this->template->js('jquery-1.8.0.min');
+		$this->template->js('jquery-ui-1.8.23.custom.min');
+		$this->template->css('Flick/jquery-ui-1.8.23.custom');
+
+		// Bootstrap CSS framework
+>>>>>>> JQuery, JQuery UI + Flick Theme and Calendar Widget added.
 		$this->template->js('common/bootstrap.min');
 		$this->template->css('common/bootstrap.min');
+
+		//CSS of mainpage
 		$this->template->css('mainPage');
 		$this->template->write_view('content', 'mainPage_view');
 		$this->template->render();
--- application/views/mainPage_view.php
+++ application/views/mainPage_view.php
@@ -11,6 +11,24 @@
 			<h1 class="title2">ERASMUS</h1>
 		</div>
 
+		<script type="text/javascript">
+			$(function(){
+
+				// Datepicker
+				$('#datepicker').datepicker({
+					inline: true
+				});
+
+				//hover states on the static widgets
+				$('#dialog_link, ul#icons li').hover(
+					function() { $(this).addClass('ui-state-hover'); },
+					function() { $(this).removeClass('ui-state-hover'); }
+				);
+
+			});
+		</script>
+		<div id="datepicker"></div>
+
 		
 	</div>
 	<div id="rightContainer">
@@ -24,6 +42,16 @@
 					<div class="button leftButton"><p>Featured Events ▼</p></div>
 					<div class="button rightButton"><p><img src="extras/img/icons/addEvent.png" class="icon">Add Event</p></div>
 				</div>
+				<div class="event"><div class="eventImg"></div></div>
+				<div class="event"><div class="eventImg"></div></div>
+				<div class="event"><div class="eventImg"></div></div>
+				<div class="event"><div class="eventImg"></div></div>
+				<div class="event"><div class="eventImg"></div></div>
+				<div class="event"><div class="eventImg"></div></div>
+				<div class="event"><div class="eventImg"></div></div>
+
+
+
 			</div>
 			<div id="rightContent">
 				<div class="bar">
