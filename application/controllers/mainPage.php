e908ade9bdde395b59e58bd39a9444eef17172c5
Carlos Baraza Haro <demon.xel@gmail.com>
2012-09-19 05:47:15 +0100

    Fix branches merge in ffile mainPage.php

--- application/controllers/mainPage.php
+++ application/controllers/mainPage.php
@@ -4,16 +4,12 @@ class mainPage extends CI_Controller {
 
 	public function index()
 	{
-<<<<<<< HEAD
-		$this->template->js('common/jquery');
-=======
 		//JQuery + JQuary UI with Flick Theme
 		$this->template->js('jquery-1.8.0.min');
 		$this->template->js('jquery-ui-1.8.23.custom.min');
 		$this->template->css('Flick/jquery-ui-1.8.23.custom');
 
 		// Bootstrap CSS framework
->>>>>>> JQuery, JQuery UI + Flick Theme and Calendar Widget added.
 		$this->template->js('common/bootstrap.min');
 		$this->template->css('common/bootstrap.min');
 
