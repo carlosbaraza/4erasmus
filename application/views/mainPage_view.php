50b9549797b0f6a124bc5dc061a5f9555ad0246f
Carlos Baraza Haro <demon.xel@gmail.com>
2012-09-19 15:46:07 +0100

    Wall post styling added

--- application/views/mainPage_view.php
+++ application/views/mainPage_view.php
@@ -88,6 +88,22 @@
 					<div class="button leftButton"><p>All ▼</p></div>
 					
 				</div>
+				<div class="wallEntry">
+					<div class="picture"></div>
+					<div class="container">
+						<p class="name">Carlos Baraza</p>
+						<p class="text">This is the first entry for trying the event description. Loren ipsum et dolor su hamed jusaras.</p>
+
+						<div class="comment">
+							<div class="picture"></div>
+							<div class="container">
+								<p class="name">Carlos Baraza</p>
+								<p class="text">This is the first entry for trying the event description. Loren ipsum et dolor su hamed jusaras.</p>
+							</div>
+						</div>
+
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
