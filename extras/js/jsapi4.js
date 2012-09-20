a837f4705d164ee2b850a66faf2d4aeb72dcbebf
ozantunca <ozantunca@computer.org>
2012-09-20 17:00:36 +0100

    conflict fix

--- extras/js/jsapi4.js
+++ extras/js/jsapi4.js
@@ -26,31 +26,32 @@
 		 	$('#loginForm').css('visibility','hidden');
 		});
 
+		// Add Event Dialog with JQuery UI Dialog
+		$.fx.speeds._default = 400;
+		$(function() {
+			$( "#addEventDialog" ).dialog({
+				autoOpen: false,
+				title: "Add a new Event",
+				resizable: "false",
+				width: "600",
+				height: "400",
+				show: "drop",
+				hide: "drop",
+				modal: "true"
+			});
+
+			$( "#addEventButton" ).click(function() {
+				$( "#addEventDialog" ).dialog( "open" );
+				return false;
+			});
+		});
+
         (function() {
           	var e = document.createElement('script'); e.async = true;
           	e.src = document.location.protocol +
           	'//connect.facebook.net/en_US/all.js';
           	document.getElementById('fb-root').appendChild(e);
-        }());
-        window.fbAsyncInit = function() {
-	        FB.init({
-	          appId: '529250917090161',
-	          cookie: true,
-	          xfbml: true,
-	          oauth: true
-	        });
-	        FB.Event.subscribe('auth.login', function(response) {
-	          window.location.reload();
-	        });
-	        FB.Event.subscribe('auth.logout', function(response) {
-	          window.location.reload();
-	        });
-	        FB.api('/<?=$fbid?>/?fields=picture.width(34).height(34)&access_token=AAAHhWdTpA3EBAOSbW67NjsAGh92UXnk0ZBpAnWCslFmFScGRaKn1a5fZBhYxRn9rMDjcsXLiaTKsfTFLq70kFdUt7T3MX6B41jiLtK1AZDZD', function(response) { 
-	        	console.log(response)
-	        	$('#loginBar p').prepend('<img src="'+ response.picture.data.url +'">')
-			});
-        };
-				   
+        }());	   
 	});
 
 
