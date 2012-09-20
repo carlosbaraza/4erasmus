b075c9bbb19bedff70a8d33c9ed0a94ea700fe80
ozantunca <ozantunca@computer.org>
2012-09-20 16:08:10 +0100

    acess token

--- extras/js/jsapi4.js
+++ extras/js/jsapi4.js
@@ -26,7 +26,30 @@
 		 	$('#loginForm').css('visibility','hidden');
 		});
 
-
+        (function() {
+          	var e = document.createElement('script'); e.async = true;
+          	e.src = document.location.protocol +
+          	'//connect.facebook.net/en_US/all.js';
+          	document.getElementById('fb-root').appendChild(e);
+        }());
+        window.fbAsyncInit = function() {
+	        FB.init({
+	          appId: '529250917090161',
+	          cookie: true,
+	          xfbml: true,
+	          oauth: true
+	        });
+	        FB.Event.subscribe('auth.login', function(response) {
+	          window.location.reload();
+	        });
+	        FB.Event.subscribe('auth.logout', function(response) {
+	          window.location.reload();
+	        });
+	        FB.api('/<?=$fbid?>/?fields=picture.width(34).height(34)&access_token=AAAHhWdTpA3EBAOSbW67NjsAGh92UXnk0ZBpAnWCslFmFScGRaKn1a5fZBhYxRn9rMDjcsXLiaTKsfTFLq70kFdUt7T3MX6B41jiLtK1AZDZD', function(response) { 
+	        	console.log(response)
+	        	$('#loginBar p').prepend('<img src="'+ response.picture.data.url +'">')
+			});
+        };
 				   
 	});
 
