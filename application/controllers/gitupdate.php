b97b837d7000879d3d5b453125c3c432616ee237
ozantunca <ozantunca@computer.org>
2012-09-25 13:41:25 +0100

    syntax fixation

--- application/controllers/gitupdate.php
+++ application/controllers/gitupdate.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Api4 extends CI_Controller {
+class Gitupdate extends CI_Controller {
 	
 	public function index() {
 		echo '<pre>' . shell_exec('git pull') . '</pre>';
