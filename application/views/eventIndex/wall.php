52e9b9cd258c7e1b98ece4b96e5bf23c5b8fdec6
Carlos Baraza Haro <demon.xel@gmail.com>
2012-10-13 02:00:25 +0100

    Facebok wall

--- application/views/eventIndex/wall.php
+++ application/views/eventIndex/wall.php
@@ -2,7 +2,7 @@
 	<div class="bar">
 		<div class="button leftButton"><p>All ▼</p></div>
 	</div>	
-
+<!--
 	<div class="wallEntry">
 		<div class="picture"></div>
 		<div class="container">
@@ -19,5 +19,10 @@
 
 		</div>
 	</div>
+-->
+
+	
+
+	<div class="fb-comments" data-href="http://www.4erasmus.com" data-num-posts="6" data-width="366"></div>
 
 </div>
