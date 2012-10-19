343a124cf6ba7df722ec66eaa5778a233ad7bee5
Carlos Baraza Haro <demon.xel@gmail.com>
2012-10-19 04:10:13 +0100

    test reverse

--- application/views/schoolProfile/schoolProfile.php
+++ application/views/schoolProfile/schoolProfile.php
@@ -22,7 +22,10 @@
 		<a class="btn schoolContentEditBtn" href="#" onclick="schoolContentEdit()"><i class="icon-pencil"></i> Edit</a>
 		<div id="schoolContent">
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada leo nec urna hendrerit viverra. Praesent ut nisi felis. Phasellus cursus lorem vitae nisi laoreet eget molestie metus egestas. Nulla mollis urna in leo dictum ut accumsan sapien feugiat. In eget euismod nisi. Donec commodo diam non quam ultrices id consectetur purus pharetra. Curabitur convallis quam sit amet lectus scelerisque commodo. Nam dapibus neque sed metus varius rutrum. Mauris gravida massa quis velit tristique sed vulputate lorem convallis. Proin quis fermentum leo. Duis posuere venenatis auctor. Morbi auctor pellentesque ante, id sollicitudin ligula egestas eget. Curabitur urna orci, molestie ut hendrerit at, dignissim eu felis.</p>
-</p>
+
+<p>Nullam volutpat nibh et neque tincidunt sed dapibus neque consectetur. In hac habitasse platea dictumst. In et justo bibendum justo scelerisque euismod et suscipit augue. Fusce metus neque, dapibus nec elementum at, iaculis quis ipsum. Ut gravida congue tempus. Sed vehicula vulputate cursus. Donec leo neque, eleifend non imperdiet ac, ultricies ut sapien. Ut scelerisque dolor quis urna vestibulum et porttitor tortor ultricies. Nam felis enim, adipiscing at gravida et, vehicula ut felis.</p>
+
+<p>Nullam dictum, nisi a scelerisque gravida, nibh mauris lacinia diam, vel imperdiet tortor magna at neque. Maecenas hendrerit porta turpis id molestie. Praesent sed lacus libero. Proin tincidunt neque ut dui commodo sit amet rutrum urna lobortis. Quisque quis sem a risus hendrerit suscipit blandit et nisi. Sed non odio dui. Etiam nec risus et quam bibendum cursus. In porta venenatis sollicitudin. Phasellus mauris enim, euismod non ornare non, eleifend non nisl. Nunc quis sapien tortor, in suscipit orci. Suspendisse lobortis ultrices velit id ornare. In dictum nibh id nibh sagittis non vulputate lacus consequat.</p>
 		</div>
 
 	</div>
